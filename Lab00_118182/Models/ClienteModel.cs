c42dcb90c25c5ca69aa84ef2c0056441ea4f7c50
DiegoBautista <da10bautista@gmail.com>
2022-02-06 14:30:12 -0600

    Creación de ClienteModel y atributos con Dataanotation

---
+++ Lab00_118182/Models/ClienteModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab00_118182.Models
+{
+    public class ClienteModel
+    {
+        [MaxLength(15)]
+        [MinLength(1)]
+        public string Nombre { get; set; }
+        [MaxLength(15)]
+        [MinLength(1)]
+        public string Apellido { get; set; }
+        [Range(1,8)]
+        public int Telefono { get; set; }
+        [MaxLength(500)]
+        public string Descripcion { get; set; }
+    }
+}
