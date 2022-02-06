ac4f81d1831622a349fe30de054906934c714f8f
DiegoBautista <da10bautista@gmail.com>
2022-02-06 14:08:39 -0600

    Creación del procyecto

---
+++ Lab00_118182/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab00_118182.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
